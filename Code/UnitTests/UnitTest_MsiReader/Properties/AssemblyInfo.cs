4cda3d3c6e2ecca1214bea3fc08687dc8ddf3d66
David COURTEL <david.courtel@gmail.com>
2019-06-02 17:45:57 +0200

    Build v1.0.1906.2

--- Code/UnitTests/UnitTest_MsiReader/Properties/AssemblyInfo.cs
+++ Code/UnitTests/UnitTest_MsiReader/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("70eb2f3c-4895-4b86-9270-c3a589f91619")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1906.2")]
+[assembly: AssemblyFileVersion("1.0.1906.2")]
